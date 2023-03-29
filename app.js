ae50bee402881e2058a6841273445666a0b59967
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-03-29 23:12:21 +0600

    Ver 1.0 functional

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const rockBtn = document.querySelector('#rock-button');
-const scissorsBtn = document.querySelector('#scissors-button');
-const paperBtn = document.querySelector('#paper-button');
+const buttons = document.querySelectorAll('.option-div img'); // 0 - rock, 1 - scissors, 2 - paper
+const buttonContainers = document.querySelectorAll('.option-div');
 const totalScore = document.querySelector('#total-score');
+const resultMessage = document.querySelector('.results-div');
 
 let playerScore = 0;
 let computerScore = 0;
@@ -9,43 +9,55 @@ const outcomes = [[1,2,0],  // 0 - loss    R S P
                   [0,1,2],  // 1 - tie     S
                   [2,0,1]];  // 2 - win    P
 
-function getComputerChoice(){
-
+function getComputerChoice() { 
   const maxOptions = 3;
-
-  return Math.floor(Math.random()*maxOptions);
+  return Math.floor(Math.random() * maxOptions);
 }
 
-function returnScore(){
-  totalScore.innerHTML=`${playerScore} - ${computerScore}`;
+function returnScore() {
+  totalScore.innerHTML = `${playerScore} - ${computerScore}`;
 }
 
-function playRound(playerValue, computerSelection){
-  
-  let roundOutcome = outcomes[playerValue][computerSelection];
-  if(roundOutcome===1)
-  return "It's a Tie!";
+function playRound(playerSelection, computerSelection) {
 
-  else if(roundOutcome===2){
+  buttonContainers.forEach(btn => {  //removing prev. borders
+    btn.classList.remove('computer-border');
+    btn.classList.remove('player-border');
+  })
+
+  let roundOutcome = outcomes[playerSelection][computerSelection];
+
+  if (roundOutcome === 1) {
+    buttonContainers[playerSelection].classList.add('player-border');
+    return "It's a Tie!"
+  }
+
+  else if (roundOutcome === 2) {
     playerScore++;
-  return "You Won!";}
+    buttonContainers[computerSelection].classList.add('computer-border');
+    buttonContainers[playerSelection].classList.add('player-border');
+    if (playerScore === 5)
+      return "You have won the game! Congrats!";
+    else return "You Won!"
+      ;
+  }
 
-  else if (roundOutcome===0){
+  else if (roundOutcome === 0) {
     computerScore++;
-  return "You Lost!";}
+    buttonContainers[computerSelection].classList.add('computer-border');
+    buttonContainers[playerSelection].classList.add('player-border');
+    if (computerScore === 5)
+      return "You have lost the game to the computer!";
+    else return "You Lost!"
+      ;
+  }
 };
 
-rockBtn.addEventListener('click',(e)=>{
-  playRound(0, getComputerChoice());
-  returnScore();
-});
-
-scissorsBtn.addEventListener('click',(e)=>{
-  playRound(1, getComputerChoice());
-  returnScore();
-});
-
-paperBtn.addEventListener('click',(e)=>{
-  playRound(2, getComputerChoice());
-  returnScore();
+buttons.forEach((button, index) => {
+  button.addEventListener('click', (e) => {
+    if (playerScore < 5 && computerScore < 5) {
+      resultMessage.innerHTML = playRound(index, getComputerChoice());
+      returnScore();
+    }
+  });
 });
